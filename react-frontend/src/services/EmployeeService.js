620e19d0a165ed9cae9935258ed6740af03ba7a9
Ubuntu <ubuntu@ip-172-31-4-157.ap-south-1.compute.internal>
2024-12-18 13:04:40 +0000

    Changes to run application on aws ec2 instance

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://13.201.191.159:7070/api/v1/employees";
 
 class EmployeeService {
 
